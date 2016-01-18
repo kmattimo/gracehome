791136aca242a873b990e05543a4e1ee973959b5
Kyle Mattimore <kmattimo@umich.edu>
2016-01-18 22:15:53 +0530

    placeholder page

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Grace Homepage!</title>
+  <meta name="description" content="Grace Care Center">
+  <meta name="author" content="Kyle Mattimore" >
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+  
+<h1> Grace Internet Homepage </h1>
+<h3> This the start of a website Kyle is creating. </h3>
+  
+  <br> 
+  <a href="https://en.wikipedia.org">English Wikipedia</a>
+  <br>
+  <a href="https://ta.wikipedia.org">Tamil Wikipedia</a>
+</body>
+</html>
