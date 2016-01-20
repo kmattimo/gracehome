24e9a32b2ad4bc83bd3d686e005a5e8b78998fd8
Kyle Mattimore <kmattimo@umich.edu>
2016-01-20 23:36:03 +0530

    some starter content

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta name="description" content="Grace Care Center">
   <meta name="author" content="Kyle Mattimore" >
   <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
-  <link rel='stylesheet' type='text/css' href='/css/site.css' />
+  <link rel='stylesheet' type='text/css' href='/css/site.css?vers=1' />
   
 </head>
 <body>
@@ -19,20 +19,51 @@
   </div>
 </div>
 
-<div class="container ">
-  <div class="row">
-      <h4 class="center"> This the start of a website Kyle is creating. </h4> 
+<div class="section">
+  <div class="container ">
+    <div class="row">
+        <h4 class="center"> This the start of a website Kyle is creating. </h4> 
+    </div>
+  </div>
+</div>
+
+<div class="section-divider">
+  <div class="container">
+    <div class="section-header">
+      Links
+    </div>
   </div>
 </div>
 
 <div class="section">
   <div class="container">
-    <div class="row fancy-text">
+    <a href="https://docs.google.com/spreadsheets/d/1_Bc2FcGZryWpkE-7H3DikLuBc1BZ22rPclLGnyKvt-8/" target="_blank">Rosetta Stone Progress Log</a>
+  </div>
+</div>
+
+<div class="section-divider">
+  <div class="container">
+    <div class="section-header">
+      Language Resources: 
+    </div>
+  </div>
+</div>
+
+<div class="section">
+  <div class="container">
+    <div class="row">
       <div class="one-half column">
-        <a target="_blank" href="https://en.wikipedia.org">English Wikipedia</a> 
+        <ul>
+          <li> <a target="_blank" href="https://en.wikipedia.org">English Wikipedia</a> </li>
+          <li> <a target="_blank" href="https://google.lk">Google Search</a> </li>
+          <li> <a target="_blank"  href="https://translate.google.com/#en/ta/Hello!">Google Translate</a> </li>
+        </ul>
       </div>
       <div class="one-half column">
-        <a target="_blank" href="https://ta.wikipedia.org">Tamil Wikipedia</a>
+        <ul>
+          <li> <a target="_blank" href="https://ta.wikipedia.org">Tamil Wikipedia</a>
+          <li> <a target="_blank" href="https://www.google.lk/?hl=ta">Google Search (in Tamil) </a> </li> 
+        </ul>
       </div>
     </div>
   </div>
@@ -41,17 +72,47 @@
 <div class="section-divider">
   <div class="container">
     <div class="section-header">
-      Stuff
+      Music Resources: 
     </div>
   </div>
 </div>
 
 <div class="section">
   <div class="container">
-    Heres some cool stuff
+    <div class="row">
+      <div class="one-half column">
+        <h5> Justinguitar</h5>
+        <a target="_blank" href="http://www.justinguitar.com/">Justinguitar Site</a>
+        <br>
+        <a target="_blank" href="http://www.justinguitar.com/en/BC-000-BeginnersCourse.php">Beginner's Guide</a> 
+      </div>
+      <div class="one-half column">
+        <h5> Other</h5>
+        <a target="_blank" href="/img/Cowboy-Chords.gif">Guitar Chords</a> 
+        <br>
+        <a target="_blank"  href="http://www.guitarfriendly.net/basic-guitar-chord-charts/">Guitar Chords Site</a> 
+      </div>
+    </div>
   </div>
 </div>
 
+<div class="section-divider">
+  <div class="container">
+    <div class="section-header">
+      Cool stuff
+    </div>
+  </div>
+</div>
+
+<div class="section">
+  <div class="container">
+    <div class="row">
+      <a href="https://www.google.lk/maps/@8.5850908,81.220053,14.07z" target="_blank"> Google Maps- Trincomalee  </a>
+      <br>
+      <a href="https://www.google.lk/maps/@42.2769361,-83.7382822,17.05z?hl=en" target="_blank"> Google Maps- University of Michigan </a>
+    </div>
+  </div>
+</div>
 
   
 </body>
