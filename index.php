27701a03a1d19f7891fc0d28a15583bb327fece5
Kyle Mattimore <kmattimo@umich.edu>
2016-01-21 21:11:21 +0530

    get flexed

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <meta name="author" content="Kyle Mattimore" >
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
-  <link rel='stylesheet' type='text/css' href='/css/site.css?vers=1' />
+  <link rel='stylesheet' type='text/css' href='/css/site.css?vers=2' />
   <link href="https://fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
   
 </head>
