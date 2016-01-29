ae1edcd32539a14ba397c8e675809948a93f8614
Kyle Mattimore <kmattimo@umich.edu>
2016-01-29 22:55:13 +0530

    now with icons

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
   <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
   <link rel='stylesheet' type='text/css' href='/css/site.css?vers=3' />
   <link href="https://fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+  <!-- <script src="https://use.fonticons.com/b0b1e96a.js"></script> -->
   
 </head>
 <body>
@@ -39,66 +40,87 @@
 
 <div class="section fancy-text">
   <div class="container">
-    <a href="https://mail.google.com" target="_blank">Gmail</a>
     
-    <a href="https://mail.google.com" target="_blank">Google</a>
+    <a  href="https://mail.google.com" target="_blank">
+      <span class="icon-gmail"></span>Gmail</a>
     
-    <a href="https://drive.google.com/drive/u/0/folders/0B2ofTcXEQyC8MlBFV3drMGcxcjQ" target="_blank">Grace Google Drive</a>
+    <a href="https://www.google.lk/?hl=en" target="_blank">
+      <span class="icon-google"></span>Google</a>
     
-    <span> <a href="http://www.wolframalpha.com/">WolframAlpha</a> </span>
+    <a href="https://drive.google.com/drive/u/0/folders/0B2ofTcXEQyC8MlBFV3drMGcxcjQ" target="_blank">
+      <span class="icon-dove-41260"></span>Grace Google Drive</a>
     
+    <a href="http://www.wolframalpha.com/">
+      <span class="icon-book"></span>WolframAlpha</a>
+  
   </div>
 </div>
 
 <div class="section-divider">
   <div class="container">
-    <h4> Language Resources </h4>
+    <h4><span class="icon-language"></span>Language Resources </h4>
   </div>
 </div>
 
 <div class="section">
   <div class="container">
-    <a target="_blank" href="https://www.typing.com/typinggames">Typing Game</a> 
-    <a target="_blank" href="https://simple.wikipedia.org/wiki/Main_Page">Simple English Wikipedia</a> 
-    <a target="_blank" href="https://en.wikipedia.org">English Wikipedia</a> 
-    <a target="_blank" href="https://ta.wikipedia.org">Tamil Wikipedia</a>
     
-    <a target="_blank" href="https://www.google.lk/?hl=ta">Google Search (in Tamil) </a>
-    <a target="_blank"  href="https://translate.google.com/#en/ta/Hello!">Google Translate</a> 
-    
+    <a target="_blank" href="https://www.typingclub.com/">
+      <span class="icon-keyboard"></span>Learn Typing Game</a> 
+    <a target="_blank" href="https://www.typing.com/typinggames">
+      <span class="icon-keyboard"></span>Typing Games Site</a> 
+    <a target="_blank" href="https://simple.wikipedia.org/wiki/Main_Page">
+      <span class="icon-wikipedia"></span>Simple English Wikipedia</a> 
+    <a target="_blank" href="https://en.wikipedia.org">
+      <span class="icon-wikipedia"></span>English Wikipedia</a> 
+    <a target="_blank" href="https://ta.wikipedia.org">
+      <span class="icon-wikipedia"></span>Tamil Wikipedia</a>
+    <a target="_blank" href="https://www.google.lk/?hl=ta">
+      <span class="icon-google"></span>Google Search (in Tamil) </a>
+    <a target="_blank"  href="https://translate.google.com/#en/ta/Hello!">
+      <span class="icon-language"></span>Google Translate</a> 
   </div>
 </div>
   
 <div class="section-divider">
   <div class="container">
-    <h4> Music Resources </h4>
+    <h4><span class="icon-music"></span>Music Resources </h4>
   </div>
 </div>
 
 <div class="section">
   <div class="container">
-    <a target="_blank" href="http://www.justinguitar.com/">Justinguitar Site</a>
-    <a target="_blank" href="http://www.justinguitar.com/en/BC-000-BeginnersCourse.php"> Justinguitar Beginner's Guide</a> 
+    <a target="_blank" href="http://www.justinguitar.com/">
+    <span class="icon-laptop"></span>Justinguitar Site</a>
+    <a target="_blank" href="http://www.justinguitar.com/en/BC-000-BeginnersCourse.php">
+    <span class="icon-videocam"></span>Justinguitar Beginner's Guide</a> 
 
     <a target="_blank" href="/img/Cowboy-Chords.gif">Guitar Chords</a> 
-    <a target="_blank"  href="http://www.guitarfriendly.net/basic-guitar-chord-charts/">More Guitar Chords</a> 
-    <a target="_blank"  href="http://www.songsterr.com/">Songsterr- look up songs</a>     <a target="_blank"  href="https://www.youtube.com/user/kylemattimore/videos">Kyle's Youtube channel</a> 
+    <a target="_blank"  href="http://www.guitarfriendly.net/basic-guitar-chord-charts/">
+      More Guitar Chords</a> 
+    <a target="_blank"  href="http://www.songsterr.com/">
+      <span class="icon-search"></span>Songsterr- look up songs</a>     
+      <a target="_blank"  href="https://www.youtube.com/user/kylemattimore/videos">
+      <span class="icon-youtube">Kyle's Youtube Channel</a> 
   </div>
 </div>
 
 <div class="section-divider">
   <div class="container">
-    <h4> Cool stuff</h4>
+    <h4> <span class="icon-book-open"></span>Cool stuff</h4>
   </div>
 </div>
 
 <div class="section">
   <div class="container">
-    <a href="https://www.pinterest.com/" target="_blank"> Pinterest- discover cool stuff </a>
+    <a href="https://www.pinterest.com/" target="_blank"> 
+      <span class="icon-pinterest"></span>Pinterest- discover cool stuff </a>
     
-    <a href="https://www.google.lk/maps/@8.5850908,81.220053,14.07z" target="_blank"> Google Maps- Trincomalee  </a>
+    <a href="https://www.google.lk/maps/@8.5850908,81.220053,14.07z" target="_blank"> 
+      <span class="icon-map"></span>Google Maps- Trincomalee  </a>
 
-    <a href="https://www.google.lk/maps/@42.2769361,-83.7382822,17.05z?hl=en" target="_blank"> Google Maps- University of Michigan </a>
+    <a href="https://www.google.lk/maps/@42.2769361,-83.7382822,17.05z?hl=en" target="_blank"> 
+      <span class="icon-map">Google Maps- University of Michigan </a>
   </div>
 </div>
 
