8e8ee3208d3b105493a7261cfe405c7f48695f98
Kyle Mattimore <kmattimo@umich.edu>
2016-04-17 15:14:46 -0500

    tamil input tools increment css version

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <meta name="author" content="Kyle Mattimore" >
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
-  <link rel='stylesheet' type='text/css' href='/css/site.css?vers=3' />
+  <link rel='stylesheet' type='text/css' href='/css/site.css?vers=4' />
   <link href="https://fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
   <!-- <script src="https://use.fonticons.com/b0b1e96a.js"></script> -->
   
